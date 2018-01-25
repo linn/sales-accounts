a7446d76d2c32c3eb1608507689dedaa96d197e5
richard phillips <richard.phillips@linn.co.uk>
2018-01-25 12:23:24 +0000

    solution pre logic with no CI

---
+++ tests/Integration/Service.Tests/NancyContextBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Linn.SalesAccounts.Service.Tests
+{
+    using Nancy.Testing;
+
+    public abstract class NancyContextBase
+    {
+        protected Browser Browser { get; set; }
+
+        protected BrowserResponse Response { get; set; }
+    }
+}
