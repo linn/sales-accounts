d94f1f8a4ec1fd25b1c52fc67885058fc56b95a0
Adam Campbell <Adam.Campbell@linn.co.uk>
2018-02-23 10:04:32 +0000

    Adding test for updating account with no address

--- src/Service.Host/client/src/config.js
+++ src/Service.Host/client/src/config.js
@@ -1,7 +1,7 @@
 ﻿const config = {
     appRoot: PROCESS.ENV.appRoot ? PROCESS.ENV.appRoot : '',
     proxyRoot: 'http://localhost:60395',
-    countryRoot:  'http://app.linn.co.uk'
+    countryRoot: 'http://app.linn.co.uk'
 }
 
 export default config;
