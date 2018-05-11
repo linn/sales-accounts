3c82fc0286c2d689796e1ee9c1c214fc48946c7d
richard phillips <richard.phillips@linn.co.uk>
2018-05-11 16:37:42 +0100

    add migration

---
+++ src/Persistence/Migrations/20180511153128_generate-id.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Linn.SalesAccounts.Persistence.Migrations
+{
+    public partial class generateid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ProposedTurnoverBands",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ProposedTurnoverBands",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+    }
+}
