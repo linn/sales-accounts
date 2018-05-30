a087e5ea0c933a85de7ff9387e0c73f3e525df74
richard phillips <richard.phillips@linn.co.uk>
2018-05-30 12:07:59 +0100

    remove incubated csv code and replace with common version.

--- src/Service/ResponseProcessors/ProposedTurnoverBandsCsvResponseProcessor.cs
+++ src/Service/ResponseProcessors/ProposedTurnoverBandsCsvResponseProcessor.cs
@@ -4,7 +4,6 @@
 
     using Linn.Common.Nancy.Facade;
     using Linn.SalesAccounts.Domain.Models;
-    using Linn.SalesAccounts.Facade.Facade.Csv;
 
     public class ProposedTurnoverBandsCsvResponseProcessor : CsvResponseProcessor<IEnumerable<ProposedTurnoverBandModel>>
     {
