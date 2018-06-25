ce35979863aa539264fdcd74c82bae7163e17f60
atcampbell <adamtoddcampbell@gmail.com>
2018-06-25 10:35:17 +0100

    add closed label (#83)

--- src/Service.Host/client/src/config.js
+++ src/Service.Host/client/src/config.js
@@ -1,9 +1,3 @@
 ﻿const config = window.APPLICATION_SETTINGS;
 
-//const config = {
-//    appRoot: PROCESS.ENV.appRoot ? PROCESS.ENV.appRoot : '',
-//    proxyRoot: PROCESS.ENV.proxyRoot ? PROCESS.ENV.proxyRoot : '',
-//    countryRoot: 'http://app.linn.co.uk'
-//}
-
 export default config;
