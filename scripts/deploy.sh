953858486a1f03784f4ade83ba09331b0e7a5c07
rosss <rosss@PC1329>
2023-09-07 11:39:59 +0100

    production salesaccounts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,9 +15,9 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
     aws s3 cp s3://$S3_BUCKET_NAME/sales-accounts/production.env ./secrets.env
 
     STACK_NAME=sales-accounts
-    APP_ROOT=http://app.linn.co.uk
-	  AUTHORITY_URI=https://www.linn.co.uk/auth/
-    PROXY_ROOT=http://app.linn.co.uk
+    APP_ROOT=
+	  AUTHORITY_URI=
+    PROXY_ROOT=
 	  ENV_SUFFIX=
   else
     # pull request based on master - deploy to sys
